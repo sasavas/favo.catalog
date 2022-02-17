9e8c537cd20df227875ac162245afd16886a8bc1
fsavas <savasusame@gmail.com>
2022-02-17 16:20:49 +0300

    fix: about us paragraphs

--- pages/AboutUs.js
+++ pages/AboutUs.js
@@ -5,11 +5,11 @@ import { useRouter } from "next/router";
 function AboutUs() {
   const { locale } = useRouter();
   const { aboutUs } = mainNavLinks[locale];
-  const ps = aboutUsPageParagraphs[locale];
+  const { paragraphs } = aboutUsPageParagraphs[locale];
 
   return (
     <BlogPage title={aboutUs}>
-      {ps.map((p) => {
+      {paragraphs.map((p) => {
         return <p key={p.length}>{p}</p>;
       })}
     </BlogPage>
