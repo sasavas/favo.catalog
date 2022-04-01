c5618f8d29aa4e2bc178eda3553c18cbf0afaf1f
usavas <savasusame@gmail.com>
2022-04-01 15:28:12 +0300

    fix: server side render the product listing

--- pages/productDetails/[productId].js
+++ pages/productDetails/[productId].js
@@ -4,7 +4,7 @@ const ProductDetails = () => {
   const router = useRouter();
   const { productId } = router.query;
 
-  return <div>Product Details: {productId}</div>;
+  return <div>Product Details: This page will be available soon</div>;
 };
 
 export default ProductDetails;
