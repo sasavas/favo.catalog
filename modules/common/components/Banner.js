c9646595de4cad342223cb3b213d514236889112
usavas <savasusame@gmail.com>
2022-06-10 17:00:04 +0300

    fix:ui separate the nav links component
    display full-page nav menu on navicon click

--- modules/common/components/Banner.js
+++ modules/common/components/Banner.js
@@ -3,21 +3,10 @@ import Container from "./Container";
 import Image from "next/image";
 import NavLinks from "./NavLinks.tsx";
 import Link from "next/link";
-import { FiMenu } from "react-icons/fi";
-import { useState, useEffect } from "react";
 
 function Banner() {
-  const [showMenu, setShowMenu] = useState(false);
-
-  useEffect(() => {
-    // const bigScreen = window.innerWidth > 768;
-    // console.log("bigScreen", bigScreen);
-
-    setShowMenu(true);
-  }, []);
-
   return (
-    <Wrapper showMenu={showMenu}>
+    <Wrapper>
       <Container>
         <div className="menu">
           <div className="logo">
@@ -32,24 +21,11 @@ function Banner() {
               </a>
             </Link>
           </div>
-          <div className="nav">
-            <FiMenu
-              className="nav-icon"
-              onClick={handleMenuIconClick}
-              size="42"
-            ></FiMenu>
-            <NavLinks
-              className={`${showMenu ? "nav-links__full_screen" : "nav-links"}`}
-            ></NavLinks>
-          </div>
+          <NavLinks></NavLinks>
         </div>
       </Container>
     </Wrapper>
   );
-
-  function handleMenuIconClick() {
-    setShowMenu(true);
-  }
 }
 
 const Wrapper = styled.div`
@@ -60,36 +36,7 @@ const Wrapper = styled.div`
   .menu {
     display: flex;
     justify-content: space-between;
-
-    .nav {
-      font-size: 1.1rem;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-
-      /** when showMenu is on then display the links and hide the icon
-          when showMenu is off then hide the links and display the icon */
-      .nav-icon {
-        display: none;
-      }
-
-      .nav-links {
-        display: block;
-      }
-    }
-  }
-
-  /** when the screen is small 
-  and the user clicks on the hamburger menu 
-  make the menu full screen */
-  @media screen and (max-width: 768px) {
-    .menu {
-      .nav {
-        .nav-icon {
-          display: block;
-        }
-      }
-    }
+    align-items: center;
   }
 `;
 
--- modules/common/components/NavLinks.tsx
+++ modules/common/components/NavLinks.tsx
@@ -1,63 +1,109 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import styled from "styled-components";
 import { mainNavLinks } from "../../../constants/strings";
 import { useRouter } from "next/router";
+import { CgClose } from "react-icons/cg";
+import { FiMenu } from "react-icons/fi";
 
-function NavLinks({ className }) {
+function NavLinks() {
   const { locale } = useRouter();
   const { homepage, catalog, aboutUs, contact } = mainNavLinks[locale];
 
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    setShowMenu(false);
+  }, []);
+
   return (
-    <Wrapper className={className}>
-      <Link href="/">
-        <a>{homepage}</a>
-      </Link>
-      <Link href="/products">
-        <a>{catalog}</a>
-      </Link>
-      <Link href="/AboutUs">
-        <a>{aboutUs}</a>
-      </Link>
-      <Link href="/Contact">
-        <a>{contact}</a>
-      </Link>
+    <Wrapper>
+      <FiMenu
+        className={`nav-icon ${showMenu ? "hide" : "show"}`}
+        onClick={openMenu}
+        size="42"
+      ></FiMenu>
+      <div className={`links ${showMenu ? "links__show" : "links__hide"}`}>
+        <Link href="/">
+          <a>{homepage}</a>
+        </Link>
+        <Link href="/products">
+          <a>{catalog}</a>
+        </Link>
+        <Link href="/AboutUs">
+          <a>{aboutUs}</a>
+        </Link>
+        <Link href="/Contact">
+          <a>{contact}</a>
+        </Link>
+        <CgClose
+          className={`close-icon ${showMenu ? "show" : "hide"}`}
+          size={36}
+          onClick={closeMenu}
+        ></CgClose>
+      </div>
     </Wrapper>
   );
+
+  function openMenu() {
+    setShowMenu(true);
+  }
+
+  function closeMenu() {
+    setShowMenu(false);
+  }
 }
 
 const Wrapper = styled.nav`
-  display: flex;
-  gap: 2rem;
+  .links {
+    display: block;
+    font-size: 1.1rem;
+    display: flex;
+    gap: 2rem;
+    justify-content: space-between;
+    align-items: flex-end;
 
-  > *:hover {
-    color: rgba(0, 0, 0, 0.6);
-    cursor: pointer;
+    > *:hover {
+      color: rgba(0, 0, 0, 0.6);
+      cursor: pointer;
+    }
+  }
+
+  .nav-icon,
+  .close-icon {
+    display: none;
   }
 
   @media screen and (max-width: 768px) {
-    /** if this component's class name is nav-links */
-    &.nav-links {
+    .show {
+      display: block;
+    }
+
+    .hide {
       display: none;
     }
 
-    /** if this component's class name is nav-links__full_screen */
-    &.nav-links__full_screen {
-      display: block;
+    .links__show {
       position: fixed;
       width: 100vw;
       height: 100vh;
       top: 0;
       left: 0;
-      background-color: aliceblue;
       z-index: 999;
+
+      background-color: white;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
 
       > :nth-child(1) {
         margin-top: 4rem;
       }
+    }
 
-      display: flex;
-      flex-direction: column;
-      align-items: center;
+    .links__hide {
+      display: none;
     }
   }
 `;
